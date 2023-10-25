babec4cffe7be3c45604a0ff4c1f904b65b9091e
Ceasor7 <josegee0707@gmail.com>
2023-10-25 11:02:15 +0300

    cali look

--- src/components/DescriptionCard.jsx
+++ src/components/DescriptionCard.jsx
@@ -1,13 +1,13 @@
 const DescriptionCard = ({ imgURL, label, subtext }) => {
   return (
     <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16'>
-      <div className='w-11 h-11 flex justify-center items-center rounded-full'>
+      <div className='w-11 h-11 mx-auto flex rounded-full'>
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
-      <h3 className='mt-5 font-palanquin text-xl leading-normal font-bold'>
+      <h3 className='mt-5 font-palanquin text-center justify-center text-xl leading-normal font-bold'>
         {label}
       </h3>
-      <p className='mt-3 break-words font-montserrat text-base leading-normal text-black'>
+      <p className='mt-3 break-words text-center justify-center font-montserrat text-base leading-normal text-black'>
         {subtext}
       </p>
     </div>
