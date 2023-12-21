096ace99d1157c6295ca4e745fba1453148f9b15
Ceasor7 <josegee0707@gmail.com>
2023-12-21 14:04:12 +0300

    commit

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,42 @@
 import emailjs from 'emailjs-com'
+import { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const Contact = () => {
+
+  const form = useRef();
+  const {isLoading, setIsLoading} = useState(false)
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    setIsLoading(true)
+
+    emailjs.sendForm('service_zf8zxfa', 'template_t4j6rw6', form.current, 'ExgvEE7Vv3Uf7xor_')
+    .then((result) => {
+      console.log(result.text);
+      toast.success("Message sent successfully")
+  }, (error) => {
+      console.log(error.text);
+      toast.error("Error sending message")
+  }
+)
+.finally(() => {
+  setIsLoading(false) // Reset loading state
+});
+    e.target.reset();
+  }
+
   return (
     <div>
-      <form className="pt-7 pl-7 pr-7 pb-7">
+      <form
+      ref={form}
+      onSubmit={sendEmail}
+      noValidate=""
+       className="pt-7 pl-7 pr-7 pb-7">
         <div className="w-full max-w-md mx-auto">
             <h3 className='font-palanquin text-center text-4xl font-bold'>
             Contact
@@ -23,7 +54,7 @@ const Contact = () => {
               required
               className="p-4 bg-gray-50 border border-gray-100"
               autoComplete="off"
-              id="name"
+              name="name"
             />
           </div>
           
@@ -39,7 +70,7 @@ const Contact = () => {
               required
               className="p-4 bg-gray-50 border border-gray-100"
               autoComplete="off"
-              id="email"
+              name="email"
             />
           </div>
           <div className="w-full flex flex-col my-4">
@@ -54,7 +85,7 @@ const Contact = () => {
               required
               className="p-4 bg-gray-50 border border-gray-100"
               autoComplete="off"
-              id="subject"
+              name="subject"
             />
           </div>
           <div>
@@ -74,8 +105,10 @@ const Contact = () => {
           <div className='mt-11'>
           <button 
           type="submit"
+          disabled={isLoading}
           className="gap-2 px-7 py-4 border font-montserrat leading-none bg-[#cd7e01] text-white border-[#cd7e01] rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#cd2d00] hover:text-white duration-300">
-          Send
+           {isLoading ? "Loading..." : "Send"}
+          <ToastContainer />
         </button>
         </div>
         </div>
