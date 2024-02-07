a19fc073d155f2388cdda090a4e6306d81a6c478
Ceasor7 <josegee0707@gmail.com>
2024-02-07 16:27:49 +0300

    commit

--- src/sections/OurTeam.jsx
+++ src/sections/OurTeam.jsx
@@ -18,32 +18,32 @@ const OurTeam = () => {
           src="/kevin.jpg"
           className="rounded-full border object-cover w-[290px] h-auto sm:h-auto sm:w-[290px]"
           />
-          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Name 1 </h3>
-          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Title 1</p>
+          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Kevin Kimani Kahuro </h3>
+          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Chief Executive Officer</p>
         </div>
         <div className="group relative flex flex-col shadow-lg rounded-md items-center cursor-pointer">
           <img
           src="/shikuku.jpg"
           className="rounded-full border object-cover w-[290px] h-auto sm:h-auto sm:w-[290px]"
           />
-          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Name 1 </h3>
-          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Title 1</p>
+          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Dr. Emmanuel Shikuku </h3>
+          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Head of Research and Training</p>
         </div>
         <div className="group relative flex flex-col shadow-lg rounded-md items-center cursor-pointer">
           <img
           src="/faith.jpg"
           className="rounded-full border object-cover w-[290px] h-auto sm:h-auto sm:w-[290px]"
           />
-          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Name 1 </h3>
-          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Title 1</p>
+          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Faith Mwende </h3>
+          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Legal and advocacy</p>
         </div>
         <div className="group relative flex flex-col shadow-lg rounded-md items-center cursor-pointer">
           <img
           src="/njoroge.jpg"
           className="rounded-full border object-cover w-[290px] h-auto sm:h-auto sm:w-[290px]"
           />
-          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'> Name 1 </h3>
-          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Title 1</p>
+          <h3 className='mt-2 text-2xl text-center leading-normal font-semibold font-roboto'>Reiner Njoroge </h3>
+          <p className='mt-2 text-center font-montserrat text-[#cd2d00] text-base leading-normal'>Admin & Research Officer</p>
         </div>
         
       </div>
